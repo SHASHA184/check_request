d3d4fadcf685de09d63d2df636368ee0a6a92a22
unknown <sashashelehan@gmail.com>
2022-03-23 18:43:43 +0200

    Commit

--- loader.py
+++ loader.py
@@ -1,5 +1,7 @@
+import os
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-TG_TOKEN = '5277464061:AAGd9o-QlfQV0qQiF6reo5MjrdGkfUOlaOU'
+TG_TOKEN = os.environ['Token']
 bot = Bot(token=TG_TOKEN, parse_mode=types.ParseMode.HTML)
 dp: Dispatcher = Dispatcher(bot, storage=MemoryStorage())
