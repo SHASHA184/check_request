b167c7e8249fc06e47a1e12d37c7685b47c81ec3
unknown <sashashelehan@gmail.com>
2022-03-23 20:03:09 +0200

    Commit

--- check.py
+++ check.py
@@ -1,18 +1,20 @@
+import os
 import sqlite3
 from datetime import datetime, timedelta
 import schedule
 import time
 from config import db_path
+from loader import bot
 
 
 def job():
-    print(0)
     db = sqlite3.connect(db_path)
     sql = db.cursor()
     time_now = datetime.now().strftime('%H:%M')
     id = sql.execute("SELECT id FROM requests WHERE action = ? AND time_check LIKE ?", (1, time_now)).fetchall()
     for i in range(len(id)):
         sql.execute("UPDATE requests SET action = ? WHERE id = ? AND time_check LIKE ?", (0, id[i][0], time_now))
+        await bot.approve_chat_join_request(chat_id=os.environ['id Канала'], user_id=id[i][0])
     db.commit()
 
 
