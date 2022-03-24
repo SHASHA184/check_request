308375711abe8742c11a34893f9b4f4bf6252098
unknown <sashashelehan@gmail.com>
2022-03-24 08:13:42 +0200

    Commit

--- check.py
+++ check.py
@@ -26,6 +26,26 @@ async def job():
 
 
 while True:
-    asyncio.create_task(job())
-    aioschedule.run_pending()
-    asyncio.sleep(60)
+    await asyncio.create_task(job())
+    await aioschedule.run_pending()
+    await asyncio.sleep(60)
+
+# async def my_sleep_func():
+#     await asyncio.sleep(random.randint(0, 5))
+#
+#
+# async def display_date(num, loop):
+#     end_time = loop.time() + 50.0
+#     while True:
+#         print("Loop: {} Time: {}".format(num, datetime.datetime.now()))
+#         if (loop.time() + 1.0) >= end_time:
+#             break
+#         await asyncio.sleep(60)
+#
+#
+# loop = asyncio.get_event_loop()
+#
+# asyncio.ensure_future(display_date(1, loop))
+# asyncio.ensure_future(display_date(2, loop))
+#
+# loop.run_forever()
