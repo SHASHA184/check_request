d2386b2b02568d770d8a3324da356ad81a441a2d
unknown <sashashelehan@gmail.com>
2022-03-26 16:20:24 +0200

    Commit

--- check.py
+++ check.py
@@ -2,12 +2,19 @@ import asyncio
 import sqlite3
 from config import db_path
 from loader import bot
-
+from asyncio.exceptions import TimeoutError
+from aiogram.utils.exceptions import NetworkError
 
 
 async def job(chat_id, user_id):
     db = sqlite3.connect(db_path)
     sql = db.cursor()
     times = sql.execute("SELECT time FROM behaviour").fetchone()[0]
-    await asyncio.sleep(times*60)
-    await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
+    await asyncio.sleep(times * 60)
+    try:
+        await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
+    except NetworkError and TimeoutError:
+        await asyncio.sleep(10)
+        await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
+
+
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from asyncio.exceptions import TimeoutError
+
 
 
 class Enter_time(StatesGroup):
