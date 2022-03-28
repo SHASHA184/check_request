f19427b88cbfe08fd00c7a34a549570fb5591f85
SHASHA184 <80709277+SHASHA184@users.noreply.github.com>
2022-03-28 16:53:37 +0300

    Commit

--- check.py
+++ check.py
@@ -14,23 +14,13 @@ async def job(chat_id, user_id):
     times = sql.execute("SELECT time FROM behaviour").fetchone()[0]
     await asyncio.sleep(times * 60)
     async def accept():
-        user_channel_status = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
-        user_status = re.findall(r"\w*", str(user_channel_status))
-        try:
-            if user_status[70] != 'left':
-                print('Уже подписан')
+        user_status = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+        print(user_status["status"])
+        if user_status["status"] != 'left':
+            print('Уже подписан')
             # Условие для "подписанных"
-            else:
-                await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
-                # Условие для тех, кто не подписан
-        except:
-            if user_status[60] != 'left':
-                print('Уже подписан')
-
-                # Условие для "подписанных"
-            else:
-                await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
-                # Условие для тех, кто не подписан
+        else:
+            await bot.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
     try:
         await accept()
     except NetworkError and TimeoutError:
